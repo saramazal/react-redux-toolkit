6b3a4b340b785fb4c4e328ce85d8e980c71ca510
Sara Mazal <32996161+saramazal@users.noreply.github.com>
2022-05-25 13:01:55 +0300

    adding removeTodo

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,10 @@ const addTodo = () => {
     }
 }
 
+const removeTodo = (todoId) => {
+  setTodos(todos.filter(todo => todo.id !== todoId))
+}
+
 return (
   <div className='input-todo container'>
     <label>
@@ -31,7 +35,7 @@ return (
         todos.map(todo => <li key={todo.id}>
           <input type="checkbox" />
           <span>{todo.text}</span>
-          <span className='delete'>&times;</span>
+          <span className='delete' onClick={() => removeTodo(todo.id)}>&times;</span>
           </li>)
       }
     </ul>
